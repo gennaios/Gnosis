a22e3f814444e235041fce4b69e06ba71ec04c7b
gennaios <gennaios@users.noreply.github.com>
2016-12-31 00:04:14 -0800

    Initial commit

---
+++ Gnosis/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Gnosis
+//
+//  Created by Gennaios on 30/12/2016.
+//  Copyright © 2016 Gennaios. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
